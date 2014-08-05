e9779cbcd88aa92c4d447a13fce96d2bd1455cfa
Andrew Kohlsmith <akohlsmith@mixdown.ca>
2014-08-05 02:14:45 -0400

    clean up UDP

--- ENC28J60-BasicWebserver.cydsn/UDP.c
+++ ENC28J60-BasicWebserver.cydsn/UDP.c
@@ -8,132 +8,81 @@
  Description : Functions to send and receive generic UDP packets.
 
 */
+
 #include "IPStackMain.h"
 
-/*******************************************************************************
-* Function Name: UDPReply
-********************************************************************************
-* Summary:
-*   Generate and send a reply to a UDP packet RX'd.
-*
-* Parameters:
-*   udppkt - pointer to the UDP packet recd.
-*   datapayload - data to be sent in the UDP Reply.
-*   payloadlen - length of the data payload to be sent.
-*
-* Returns:
-*   TRUE(0)- if the UDP Reply was successfully sent.
-*   FALSE(1) - if the UDP Reply was not successful in transmission.
-*******************************************************************************/
-int UDPReply(UDPPacket* udp, uint8_t *payload, uint16_t payloadlen)
+/* respond to a UDP packet. udp is the rx'd packet, data/len are the payload to send back */
+int UDPReply(UDPPacket* udp, uint8_t *data, uint16_t len)
 {
-    uint16 port;
+	uint16 port;
 
-    /*Swap the MAC Addresses in the ETH header*/
-    memcpy( udp->udp.ip.eth.DestAddrs, udp->udp.ip.eth.SrcAddrs, 6);
-    memcpy( udp->udp.ip.eth.SrcAddrs, deviceMAC,6 );
+	/* Swap the MAC/IP Addresses */
+	memcpy(udp->udp.ip.eth.DestAddrs, udp->udp.ip.eth.SrcAddrs, sizeof(macaddr_t));
+	memcpy(udp->udp.ip.eth.SrcAddrs, deviceMAC, sizeof(macaddr_t));
+	memcpy(udp->udp.ip.dest, udp->udp.ip.source, sizeof(ipaddr_t));
+	memcpy(udp->udp.ip.source, deviceIP, sizeof(ipaddr_t));
 
-    /*Swap the IP Addresses in the IP header*/
-    memcpy( udp->udp.ip.dest, udp->udp.ip.source,4);
-    memcpy( udp->udp.ip.source, deviceIP,4);
+	/* Swap the ports */
+	port = udp->udp.sourcePort;
+	udp->udp.sourcePort = udp->udp.destPort;
+	udp->udp.destPort = port;
 
-    /*Swap the ports*/
-    port=udp->udp.sourcePort;
-    udp->udp.sourcePort=udp->udp.destPort;
-    udp->udp.destPort=port;
+	/* zero the checksums */
+	udp->udp.chksum = 0;
+	udp->udp.ip.chksum = 0;
 
-    /*zero the checksums*/
-    udp->udp.chksum=0x00;
-    udp->udp.ip.chksum=0x00;
+	/* write in the correct lengths */
+	udp->udp.len = htons(sizeof(UDPhdr) - sizeof(IPhdr) + len);
+	udp->udp.ip.len = htons(sizeof(UDPhdr) + len - sizeof(EtherNetII));
 
-    /*write in the correct lengths*/
-    udp->udp.len=(sizeof(UDPhdr)-sizeof(IPhdr))+payloadlen;
-    udp->udp.ip.len=(sizeof(UDPhdr)+payloadlen)-sizeof(EtherNetII);
+	/* clear the old payload and copy in the new */
+	memset(udp->Payload, 0, ntohs(udp->udp.len) - 8);
+	memcpy(udp->Payload, data, len);
 
-    /*clear the old payload*/
-    memset(udp->Payload,0x00, (udp->udp.len)-8 );
+	/* calculate the checksums */
+	udp->udp.ip.chksum = htons(checksum(((uint8_t *)udp) + sizeof(EtherNetII), sizeof(IPhdr) - sizeof(EtherNetII), CK_IP));
+	udp->udp.chksum = htons(checksum(&udp->udp.ip.source, 16 + len, CK_UDP));
 
-    /*copy in the payload*/
-    memcpy(udp->Payload,payload,payloadlen);
-
-    /*do the checksums.*/
-    udp->udp.ip.chksum = htons(checksum(((uint8_t *)udp) + sizeof(EtherNetII), sizeof(IPhdr) - sizeof(EtherNetII), CK_IP));
-    udp->udp.chksum = htons(checksum(&udp->udp.ip.source, 16 + payloadlen, CK_UDP));
-
-    /*Send the packet.*/
-    return tx_packet(udp, sizeof(UDPhdr) + payloadlen);
+	return tx_packet(udp, sizeof(UDPhdr) + len);
 }
 
-/*******************************************************************************
-* Function Name: UDPSend
-********************************************************************************
-* Summary:
-*   Generate and send a UDP packet with data.
-*   You may edit the UDP Port number(that will be used as the source port)
-*   in "globals.c".Default is 1200.
-*
-* Parameters:
-*   targetIP - IP address to send the UDP packet to.
-*   targetPort - Port to direct the UDP packet to.
-*   datapayload - data to be sent in the UDP packet.
-*   payloadlen - length of the data payload to be sent.
-*
-* Returns:
-*   TRUE(0)- if the UDP packet was successfully sent.
-*   FALSE(1) - if the UDP packet was not successful in transmission.
-*******************************************************************************/
-int UDPSend(ipaddr_t targetIP, uint16_t targetPort, uint8_t *payload, uint16_t payloadlen)
+
+/* send a UDP packet */
+int UDPSend(ipaddr_t dst, uint16_t dport, uint8_t *data, uint16_t len)
 {
-    UDPPacket udp;
+	UDPPacket udp;
 
-    /*Setup the IP part*/
-    SetupBasicIPPacket(&udp, PROTO_UDP, targetIP);
-    udp.udp.ip.flags = 0x0;
+	memset(&udp, 0, sizeof(udp));
+	SetupBasicIPPacket(&udp, PROTO_UDP, dst);
 
-    /*Setup the ports*/
-    udp.udp.sourcePort=UDPPort;
-    udp.udp.destPort=targetPort;
+	udp.udp.sourcePort = htons(UDPPort);
+	udp.udp.destPort = htons(dport);
 
-    /*Zero the checksums*/
-    udp.udp.chksum=0x00;
-    udp.udp.ip.chksum=0x00;
+	/*Write in the correct lengths*/
+	udp.udp.len = htons(sizeof(UDPhdr) - sizeof(IPhdr) + len);
+	udp.udp.ip.len = htons(sizeof(UDPhdr) + len - sizeof(EtherNetII));
 
-    /*Write in the correct lengths*/
-    udp.udp.len=(sizeof(UDPhdr)-sizeof(IPhdr))+payloadlen;
-    udp.udp.ip.len=(sizeof(UDPhdr)+payloadlen)-sizeof(EtherNetII);
+	memcpy(udp.Payload, data, len);
 
-    /*Copy in the payload*/
-    memcpy(udp.Payload,payload,payloadlen);
+	/* calculate the checksums */
+	udp.udp.ip.chksum = htons(checksum(((uint8_t *)&udp) + sizeof(EtherNetII), sizeof(IPhdr) - sizeof(EtherNetII), CK_IP));
+	udp.udp.chksum = htons(checksum(&udp.udp.ip.source, 16 + len, CK_UDP));
 
-    /*Do the checksums.*/
-    udp.udp.ip.chksum = htons(checksum((uint8_t *)&udp + sizeof(EtherNetII), sizeof(IPhdr) - sizeof(EtherNetII), CK_IP));
-    udp.udp.chksum = htons(checksum(&udp.udp.ip.source, 16 + payloadlen, CK_UDP));
-
-    /*Send the packet!*/
-    return tx_packet(&udp, sizeof(UDPhdr) + payloadlen);
+	return tx_packet(&udp, sizeof(UDPhdr) + len);
 }
 
-/*******************************************************************************
-* Function Name: UDP_ProcessIncoming
-********************************************************************************
-* Summary:
-*   This will be called in GetPacket,when a UDP packet is recd.You may process the data
-*   carried by that incoming UDP packet in this function,and then call UDPReply to respond.
-*
-* Parameters:
-*   incomingpacket - packet whose data payload is to be processed and replied to.
-* Returns:
-*   Nothing.
-*******************************************************************************/
-void UDP_ProcessIncoming(UDPPacket* incomingpacket){
-	unsigned char* datapointer=(unsigned char*)incomingpacket+sizeof(UDPhdr);
+/* called by GetPacket, we can spin off and process different UDP packets in here */
+void UDP_ProcessIncoming(UDPPacket *udp)
+{
+	uint8_t *data;
 
-	if(strncmp(datapointer,"Invoke.",sizeof("Invoke."))==0){
-		UDPReply(incomingpacket,"Hello World",sizeof("Hello World"));
-	}else{
-		UDPReply(incomingpacket,"Access Denied.",sizeof("Access Denied."));
-	}
+	data = (uint8_t *)udp + sizeof(UDPhdr);
 
+		if(strncmp(data,"Invoke.",sizeof("Invoke."))==0){
+			UDPReply(udp,"Hello World",sizeof("Hello World"));
+		}else{
+			UDPReply(udp,"Access Denied.",sizeof("Access Denied."));
+		}
 }
 
 /* [] END OF FILE */
